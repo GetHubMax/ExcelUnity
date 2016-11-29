d01d9b024c45a7d2ea1e69d56220c86aed82804d
GetHubMax <max.harry@hotmail.com>
2016-11-30 12:26:26 +1300

    Added Test ods and sc.

---
+++ Assets/TestSc.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSc : MonoBehaviour {
+	//may need setter methods
+	public int IntTest;
+	public double DoubleTest;
+	public string StringTest;
+	public string n;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetInt(int v){
+		IntTest = v;
+	}
+
+	public void SetDouble(double v){
+		DoubleTest = v;
+	}
+
+	public void SetString(string v){
+		StringTest= v;
+	}
+
+
+	public void Name(string v){
+		n = v;
+	}
+}
